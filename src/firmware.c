4967593c539ba3d65d954d3d765f529aa85168a8
Daniel Laszlo Sitzer <dlsitzer@gmail.com>
2017-10-09 22:02:08 +0200

    Added debug output to initialization in main().

--- src/firmware.c
+++ src/firmware.c
@@ -1,10 +1,16 @@
+#include "uart.h"
 #include "lcd.h"
 
 int main(void)
 {
+	uart_init();
+	uart_puts("UART ready\r\n");
+
+	uart_puts("LCD init ...\r\n");
 	lcd_init();
-	lcd_init();
-	lcd_puts("Hello!");
+	uart_puts("LCD ready\r\n");
+
+	uart_puts("init done\r\n");
 	while (1)
 		;
 	return 0;
